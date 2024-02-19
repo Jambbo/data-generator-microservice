f9fe5ad6fed3e8006642fccbd4c0e0a55fa77edb
Hlib Chibichian <chibgleb99@gmail.com>
2024-02-19 11:29:29 +0000

    s

---
+++ src/main/java/com/example/datageneratormicro/DataGeneratorMicroApplication.java
@@ -0,0 +1,13 @@
+package com.example.datageneratormicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGeneratorMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataGeneratorMicroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datageneratormicro/DataGeneratorMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datageneratormicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGeneratorMicroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
